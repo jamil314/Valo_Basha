8c23354623557d0ce00a0b7aa468425a4a5179ef
Jamil314 <jamil31415926@gmail.com>
2021-06-20 01:13:39 +0600

    added developer information and updated some existing fragments

--- app/src/main/java/com/example/valo_basha/gmap.java
+++ app/src/main/java/com/example/valo_basha/gmap.java
@@ -2,21 +2,36 @@ package com.example.valo_basha;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class gmap extends Fragment {
+    Location currentLocation;
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private boolean locationPermission = false;
+    private FusedLocationProviderClient locationProviderClient;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -31,27 +46,66 @@ public class gmap extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            LatLng sylhet = new LatLng(24.894802, 91.869034);
+            googleMap.addMarker(new MarkerOptions().position(sylhet).title("Marker in Sylhet"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sylhet));
+
         }
     };
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_gmap, container, false);
-    }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+        @Nullable
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_gmap, container, false);
         }
-    }
+
+
+        private void getLocationPermission() {
+            String[] permission = {FINE_LOCATION, COURSE_LOCATION};
+
+            if (ContextCompat.checkSelfPermission(this.getActivity().getApplicationContext(), FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this.getActivity().getApplicationContext(), COURSE_LOCATION) //this.getActivity().getApplicationContext()
+                        == PackageManager.PERMISSION_GRANTED) {
+                    locationPermission = true;
+                } else {
+                    ActivityCompat.requestPermissions(getActivity(), permission, 1234);
+                }
+            } else {
+                ActivityCompat.requestPermissions(getActivity(), permission, 1234);
+            }
+
+        }
+
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            locationPermission = false;
+
+            switch (requestCode) {
+                case 1234: {
+                    if (grantResults.length > 0) {
+                        for (int i = 0; i < grantResults.length; i++) {
+                            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                                locationPermission = false;
+                                return;
+                            }
+                        }
+                        locationPermission = true;
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            SupportMapFragment mapFragment =
+                    (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+            if (mapFragment != null) {
+                mapFragment.getMapAsync(callback);
+            }
+        }
 }
