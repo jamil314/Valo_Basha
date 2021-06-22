4a89bd2262fef50ed9bddfe5737fa420b1d32ce9
rafid <hasan65@student.sust.edu>
2021-06-22 13:04:18 +0600

    added marker on map

--- app/src/main/java/com/example/valo_basha/MainActivity.java
+++ app/src/main/java/com/example/valo_basha/MainActivity.java
@@ -4,6 +4,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -17,14 +23,17 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap gMap;
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment supportMapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.google_map);
+        supportMapFragment.getMapAsync(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -55,5 +64,27 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap= googleMap;
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                //creating marker
+                MarkerOptions markerOptions = new MarkerOptions();
+                //set marker position
+                markerOptions.position(latLng);
+                // set latitude and longitude on marker
+                markerOptions.title(latLng.latitude+" : "+latLng.longitude);
+                // clear previously clicked position
+                gMap.clear();
+                // zoom the marker
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                // add marker on map
+                gMap.addMarker(markerOptions);
+            }
+        });
     }
 }
