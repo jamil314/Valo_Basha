a5924e35b0186f0f5c1f3e3fb9d8926ed3f41a65
Jamil314 <jamil31415926@gmail.com>
2021-06-29 20:23:21 +0600

    Center location ca be called anytime: [yet to fix search]

--- app/src/main/java/com/example/valo_basha/map_container.java
+++ app/src/main/java/com/example/valo_basha/map_container.java
@@ -1,12 +1,34 @@
 package com.example.valo_basha;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Looper;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +36,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class map_container extends Fragment {
+
+    Button centreButton;
+    FusedLocationProviderClient client;
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -38,6 +65,7 @@ public class map_container extends Fragment {
      */
     // TODO: Rename and change types and number of parameters
     public static map_container newInstance(String param1, String param2) {
+        Log.d("JAMIL", "map_container -> (;;)");
         map_container fragment = new map_container();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
@@ -48,6 +76,7 @@ public class map_container extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.d("JAMIL", "map_container -> onCreat");
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
@@ -58,7 +87,95 @@ public class map_container extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d("JAMIL", "map_container -> onCreatView");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map_container, container, false);
+        View view = inflater.inflate(R.layout.fragment_map_container, container, false);
+
+
+         centreButton = view.findViewById(R.id.centreButton);
+            client  = LocationServices.getFusedLocationProviderClient(getActivity());
+            centreButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(ContextCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED &&
+                            ContextCompat.checkSelfPermission(getActivity(),
+                                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                                    ==PackageManager.PERMISSION_GRANTED){
+                        getLocation();
+                    }else{
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                                ,Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+                    }
+
+                }
+            });
+
+
+        return view;
+
+
+
+
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull  int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100 && grantResults.length>0 && grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED){
+            getLocation();
+        }else{
+            Toast.makeText(getActivity()
+                    ,"PERMISSION DENOED", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void getLocation(){
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            client.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+
+                    if(location != null){
+                        LatLng qpos = new LatLng(location.getLatitude(), location.getLongitude());
+                        gmap.moveTo(qpos, (float) 15.0);
+                        gmap.moveMarker(qpos);
+                        Log.d("JAMIL", "YAY!! "+String.valueOf(location.getLatitude())+" : "
+                                +String.valueOf(location.getLongitude()));
+                    } else {
+                        Log.d("JAMIL", "NO LUCK");
+                        LocationRequest locationRequest = new LocationRequest()
+                                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                                .setInterval(10000)
+                                .setFastestInterval(1000)
+                                .setNumUpdates(1);
+                        LocationCallback locationCallback = new LocationCallback() {
+                            @Override
+                            public void onLocationResult(@NonNull LocationResult locationResult) {
+                                //super.onLocationResult(locationResult);
+                                Location location1 = locationResult.getLastLocation();
+                                LatLng qpos = new LatLng(location.getLatitude(), location.getLongitude());
+                                gmap.moveTo(qpos, (float) 15.0);
+                                gmap.moveMarker(qpos);
+                                Log.d("JAMIL", "YAY!! "+String.valueOf(location.getLatitude())+" : "
+                                        +String.valueOf(location.getLongitude()));
+                            }
+                        };
+                        client.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+                    }
+                }
+            });
+        }else{
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+    }
+
+
+
+
 }
