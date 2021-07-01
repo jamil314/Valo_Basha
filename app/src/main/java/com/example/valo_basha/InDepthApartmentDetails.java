41203424a3bc5306a7a953ff78705a5781c872be
Jamil314 <jamil31415926@gmail.com>
2021-07-02 00:28:19 +0600

    Added dynamic pictures in apartment details; added report option.

--- app/src/main/java/com/example/valo_basha/InDepthApartmentDetails.java
+++ app/src/main/java/com/example/valo_basha/InDepthApartmentDetails.java
@@ -1,14 +1,31 @@
 package com.example.valo_basha;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class InDepthApartmentDetails extends AppCompatActivity {
+import com.smarteist.autoimageslider.SliderView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class InDepthApartmentDetails extends AppCompatActivity {
+    int i=0, n=7;
+    Button report, right, left;
+    ImageView imageView;
+    int images[] = new int[7];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +33,7 @@ public class InDepthApartmentDetails extends AppCompatActivity {
         Intent intent = getIntent();
         Apartment apartment = intent.getParcelableExtra("apartment");
         getSupportActionBar().setTitle(apartment.name);
+        getSupportActionBar().setHomeButtonEnabled(true);
         TextView owner = findViewById(R.id.owner);
         TextView area = findViewById(R.id.area);
         TextView bedroom = findViewById(R.id.bedroom);
@@ -47,5 +65,42 @@ public class InDepthApartmentDetails extends AppCompatActivity {
         }
         available_floors.setText(floors);
         mobile_no.setText(apartment.contactInfo);
+
+
+        report = findViewById(R.id.btn_report);
+        right = findViewById(R.id.btn_right);
+        left = findViewById(R.id.btn_left);
+        imageView = findViewById(R.id.image_view);
+        images[0]= R.drawable.apartment_entrance;
+        images[1]= R.drawable.apartment_with_furniture;
+        images[2]= R.drawable.apartment_with_furniture_2;
+        images[3]= R.drawable.apartment_with_furniture_3;
+        images[4]= R.drawable.apartment_empty;
+        images[5]= R.drawable.apartment_empty_2;
+        images[6]= R.drawable.apartment_view;
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i=(i+1)%n;
+                imageView.setImageResource(images[i]);
+                Log.d("JAMIL", "i++");
+            }
+        });
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i=(i-1+n)%n;
+                imageView.setImageResource(images[i]);
+                Log.d("JAMIL", "i--");
+            }
+        });
+        report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ri = new Intent(getApplicationContext(), Report_activity.class);
+                startActivity(ri);
+            }
+        });
+
     }
 }
