ac249ce32470c9fa27237d139f83e09472fe06db
Jamil314 <jamil31415926@gmail.com>
2021-07-06 15:07:15 +0600

    connected to firebase realtime data base, added buildings will be stored at https://maaaaap-default-rtdb.asia-southeast1.firebasedatabase.app/

--- app/src/main/java/com/example/valo_basha/NewDetails.java
+++ app/src/main/java/com/example/valo_basha/NewDetails.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -17,29 +18,43 @@ import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class NewDetails extends AppCompatActivity {
     SeekBar skbed, skbath;
     TextView bed, bath;
-    EditText t_floors;
+    EditText t_floors, rent, area;
+    CheckBox furniture;
     LinearLayout layoutlist;
-    Button add, gallery, camera;
+    Button add, gallery, camera, proceed;
     ImageView imageView;
     LinearLayout imagelist;
+    Apartment apartment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_details);
+        Log.d("JAMIL", "Opened intent");
+        Intent intent = getIntent();
+        apartment = intent.getParcelableExtra("apartment");
         getSupportActionBar().setTitle("Fill in all the details");
+        Log.d("JAMIL", "passed data");
         skbed = findViewById(R.id.seekBar_bedroom);
         bed = findViewById(R.id.bedroom_count);
         skbath = findViewById(R.id.seekBar_bathroom);
         bath = findViewById(R.id.bathroom_count);
+        t_floors = findViewById(R.id.t_floors);
+        rent = findViewById(R.id.rent);
+        area = findViewById(R.id.area);
+        furniture = findViewById(R.id.furniture);
         layoutlist = findViewById(R.id.layout);
         add = findViewById(R.id.add_floors);
         gallery = findViewById(R.id.pic_from_gallery);
         camera = findViewById(R.id.pic_from_camera);
         imagelist = findViewById(R.id.image_container);
+        proceed = findViewById(R.id.btn_proceed_to_place);
         skbed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
@@ -96,6 +111,22 @@ public class NewDetails extends AppCompatActivity {
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent, "select picture"), 1);
+            }
+        });
+
+        proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                apartment.bedrooms = Integer.parseInt(bed.getText().toString());
+                apartment.bathrooms = Integer.parseInt(bath.getText().toString());
+                apartment.rent = Integer.parseInt(rent.getText().toString());
+                apartment.totalFloors = Integer.parseInt(t_floors.getText().toString());
+                apartment.area = Double.parseDouble(area.getText().toString());
+                apartment.furniture = furniture.isChecked();
+                DatabaseReference mDatabase;
+                mDatabase = FirebaseDatabase.getInstance("https://maaaaap-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
+                Log.d("JAMIL", String.valueOf(mDatabase));
+                mDatabase.child(apartment.name).setValue(apartment);
             }
         });
     }
